58f81f6a8da198398f9428b0c482daf156944b6e
Koorosh <korosh.roohi9731@gmail.com>
2023-12-18 15:47:57 -0500

    Changing the checkpoint file.

--- configs/_base_/default_runtime.py
+++ configs/_base_/default_runtime.py
@@ -20,5 +20,5 @@ vis_backends = [dict(type='LocalVisBackend')]
 visualizer = dict(type='ActionVisualizer', vis_backends=vis_backends)
 
 log_level = 'INFO'
-load_from = "/cluster/home/t125959uhn/mmaction2/checkpoints/slowfast_r50_8xb8-8x8x1-256e_kinetics400-rgb_20220818-1cb6dfc8.pth"
+load_from = "/cluster/home/t125959uhn/mmaction2/checkpoints/slowfast_r101_8xb8-8x8x1-256e_kinetics400-rgb_20220818-9c0e09bd.pth"
 resume = False
